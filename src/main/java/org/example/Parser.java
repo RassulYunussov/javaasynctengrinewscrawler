bbc40d39d4c1f76d6d93f267eec9e2fd7d974a52
Rassul Yunussov <yunussov@gmail.com>
2020-06-09 19:04:15 +0600

    streams utilization refactored

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -22,45 +22,45 @@ import static java.util.stream.Collectors.toList;
 public class Parser {
     private CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
     private ConcurrentHashMap<String, Page> pages = new ConcurrentHashMap<>();
-    private ExecutorService executor = Executors. newCachedThreadPool();
+    private ExecutorService executor = Executors.newFixedThreadPool(100);
 
     public void crawlUrls(Iterable<String> urls) throws IOException, InterruptedException, ExecutionException {
         client.start();
         crawl(urls);
+        executor.shutdown();
         client.close();
     }
 
-    private CompletableFuture<Void> processHttpResponse(String url, Future<HttpResponse> futureResponse)
+    private CompletableFuture<Void> processHttpResponse(String url, HttpResponse httpResponse)
     {
-        try
-        {
-            var response = futureResponse.get();
-            HttpEntity entity = response.getEntity();
-            String responseString = null;
-            try {
+        if(httpResponse!=null) {
+            try
+            {
+                HttpEntity entity = httpResponse.getEntity();
+                String responseString = null;
                 responseString = EntityUtils.toString(entity, "UTF-8");
                 Page p = new Page(url, responseString);
                 if (pages.putIfAbsent(url, p) == null) {
-                    System.out.println(url);
-                    System.out.println(pages.size());
+                    System.out.println("URL:{"+url+"} PROCESSED BY THREAD: "+Thread.currentThread().getId());
+                    System.out.println("CURRENT DICTIONARY SIZE: "+pages.size());
                     return CompletableFuture.supplyAsync(() -> crawl(getUrls(p)), executor);
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
-        return CompletableFuture.supplyAsync(()->crawl(List.of()), executor);
+        return CompletableFuture.completedFuture(null);
     }
     private Void crawl(Iterable<String> Urls) {
         var result = StreamSupport.stream(Urls.spliterator(),false)
                       .map(url->new Object [] {url, client.execute(new HttpGet(url), null)})
-                      .collect(toList())
-                      .parallelStream()
-                      .map(tuple->processHttpResponse((String)tuple[0],(Future<HttpResponse>)tuple[1]))
+                      .map(tuple->CompletableFuture.supplyAsync(()-> {
+                          try {
+                              HttpResponse response = ((Future<HttpResponse>)tuple[1]).get();
+                              return new Object[] {tuple[0], response};
+                          } catch( Exception e) { return new Object []{tuple[0], null}; }
+                          }, executor))
+                      .map(tupleCompletableFuture->tupleCompletableFuture.thenCompose((tuple)->processHttpResponse((String)tuple[0],(HttpResponse)tuple[1])))
                       .toArray(size-> new CompletableFuture[size]);
         CompletableFuture.allOf(result).join();
         return null;
